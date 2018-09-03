7f4301abbbdb710e938b159921019657d91ee97e
ABycroft <42327894+ABycroft@users.noreply.github.com>
2018-09-03 23:03:09 +1200

    Fixed jitter of enemy
    All that needed to be changed to stop the jitter on the collision was changing the Update() to FixedUpdate. Took me a while to figure this out though. Still has a couple issues with jittering if going into a wall with the enemy colliding into you.

---
+++ EnemyFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Timothy Serrano: 1394556
+//Andrew Bycroft: 16948980
+
+public class EnemyFollow : MonoBehaviour {
+
+    public Transform target;
+    public float speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //Sets the enemy to find the game object named "Hero" aka our character
+        target = GameObject.FindGameObjectWithTag("Hero").GetComponent<Transform>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        //Moves the enemies position towards the target("Hero") against a fixed speed
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+	}
+}
