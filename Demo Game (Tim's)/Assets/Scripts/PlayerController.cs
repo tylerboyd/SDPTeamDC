050970f6b6b030b0e230b3eed1ab9eab43b7fcb1
ABycroft <42327894+ABycroft@users.noreply.github.com>
2018-09-20 16:54:49 +1200

    removed animation from script

--- Demo Game (Tim's)/Assets/Scripts/PlayerController.cs
+++ Demo Game (Tim's)/Assets/Scripts/PlayerController.cs
@@ -14,9 +14,9 @@ public class PlayerController : MonoBehaviour {
     void Start()
     {
         //rb2d = GetComponent<Rigidbody2D>();
-        animate = GetComponent<Animator>();
-//        joystick = FindObjectOfType<Joystick>();
-//        joybutton = FindObjectOfType<Joybutton>();
+        //animate = GetComponent<Animator>();
+        joystick = FindObjectOfType<Joystick>();
+        joybutton = FindObjectOfType<Joybutton>();
     }
 
     //Always use GetAxisRaw otherwise object will slide
@@ -26,34 +26,34 @@ public class PlayerController : MonoBehaviour {
         //rb2d.velocity = new Vector3(joystick.Horizontal * 5.0f,
          //   rb2d.velocity.y, joystick.Vertical * 5.0f);
 
-            float moveHorizontal = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
-            float moveVertical = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+            float moveHorizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+            float moveVertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
 
-            if (Input.GetKey(KeyCode.A)) 
-            {
-                animate.SetInteger("Direction", 1);
-            }
+           // if (Input.GetKey(KeyCode.A)) 
+           // {
+           //     animate.SetInteger("Direction", 1);
+           // }
 
-            if (Input.GetKey(KeyCode.W))
-            {
-                animate.SetInteger("Direction", 2);
-            }
+           // if (Input.GetKey(KeyCode.W))
+           // {
+           //     animate.SetInteger("Direction", 2);
+           // }
 
-            if (Input.GetKey(KeyCode.D))
-           {
-                animate.SetInteger("Direction", 3);
-           }
+           // if (Input.GetKey(KeyCode.D))
+           //{
+           //     animate.SetInteger("Direction", 3);
+           //}
 
-            if (Input.GetKey(KeyCode.S))
-            {
-                animate.SetInteger("Direction", 4);
-            }
+           // if (Input.GetKey(KeyCode.S))
+           // {
+           //     animate.SetInteger("Direction", 4);
+           // }
 
             transform.Translate(new Vector3(moveHorizontal, moveVertical));
 
 
-        animate.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
-        animate.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        //animate.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        //animate.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
     }
 }
 
