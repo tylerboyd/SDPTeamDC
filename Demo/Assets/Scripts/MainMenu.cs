c4c5f443390b81db0074abb480a2e433ae88a4d1
tylerboyd <36950595+tylerboyd@users.noreply.github.com>
2018-09-20 16:09:27 +1200

    Demo project update
    Added Menu functionality

---
+++ Demo/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Tyler Boyd: 16951977
+
+public class MainMenu : MonoBehaviour {
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
+
+
---
+++ Demo/Assets/Scripts/PlayerScore.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScore : MonoBehaviour
+{
+
+    private int count;
+    public Text countText;
+
+    void Start()
+    {
+        count = 0;
+        SetCountText();
+    }
+
+    void OnTriggerEnter(Collider Enemy)
+    {
+        if (Enemy.gameObject.CompareTag("Enemy 1"))
+        {
+            Enemy.gameObject.SetActive(false);
+            //Enemy.gameObject.Destroy();
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void AddScore()
+    {
+        count = count + 1;
+        SetCountText();
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Score: " + count.ToString();
+    }
+}
