dc2f40bcea7524049d350e36af02e306b7f49dc7
Tarran O'Shaughnessy <tarran.oshaughnessy@gmail.com>
2018-09-20 14:31:39 +1200

    Implementation of scores on new design
    High score and gold funtionality added to the new design, alongside save and load functions that are just awaiting implementation.

---
+++ Demo/Assets/Scripts/CanvasDontDestroy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasDontDestroy : MonoBehaviour {
+
+    public static GameObject rootCanvas;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
