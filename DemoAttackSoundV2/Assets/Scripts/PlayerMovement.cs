6fa085086930158df4bdb243a9d7889dc4773c7a
larry <larry.zhao@outlook.co.nz>
2018-09-22 11:05:18 +1200

    Push

---
+++ DemoAttackSoundV2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.UI;
+
+//Timothy Serrano: 1394556
+//Andrew Bycroft: 16948980
+//Larry Zhou:
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb2D;
+    Animator anim;
+    public float speed;
+    public Button m_attackButton;
+
+    //The delay between attacking time, so animation is not spammed.
+    private bool attacking;
+    public float attackTime;
+    private float attackTimeCounter;
+
+    public Transform attackPos;
+    public LayerMask whatIsEnemies;
+    public float attackRange;
+    public int damage;
+
+    public AudioClip attackSound1;
+    public AudioClip attackSound2;
+    public AudioClip walkSound1;
+    public AudioClip walkSound2;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        Button attackButton = m_attackButton.GetComponent<Button>();
+        attackButton.onClick.AddListener(TaskOnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //FOR JOYSTICK RUN THIS LINE (Must be on android build settings)
+        Vector2 movement_vector = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical"));
+
+        //FOR WASD RUN THIS LINE 
+        //Vector2 movement_vector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        if (!attacking)
+        {
+            if (movement_vector != Vector2.zero)
+            {
+                anim.SetBool("isWalking", true);
+                /*
+                 * These two lines of code update the float parameters for the animation,
+                 * it is what is causing the player to NOT face in the direction after running
+                 * but to face back to idle_down
+                 * 
+                */
+                anim.SetFloat("MoveX", movement_vector.x);
+                anim.SetFloat("MoveY", movement_vector.y);
+
+                anim.SetBool("isAttacking", false);
+            }
+            else
+            {
+                anim.SetBool("isWalking", false);
+                SoundManager.Instance.RandomPlayWalkingSource(walkSound1, walkSound2);
+            }
+
+            rb2D.MovePosition(rb2D.position + movement_vector * Time.deltaTime * speed);
+
+        }
+        if (attackTimeCounter <= 0)
+        {
+            attacking = false;
+            anim.SetBool("isAttacking", false);
+
+            //Space to attack
+            if (Input.GetKeyDown(KeyCode.Space) && !attacking)
+            {
+                attackTimeCounter = attackTime;
+                attacking = true;
+                rb2D.velocity = Vector2.zero;
+                anim.SetBool("isAttacking", true);
+                //THE CHANGES TO WORKING CODE
+
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+                for(int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<EnemyFollow>().TakeDamage(damage);
+                }
+            }
+            
+        }
+        else if(attackTimeCounter > 0)
+        {
+            //timeBtwAttack from blackthornprod
+            SoundManager.Instance.RandomPlayAttackSource(attackSound1, attackSound2);
+            attackTimeCounter -= Time.deltaTime;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+
+    //Function for attack button
+    public void TaskOnClick()
+    {
+        attackTimeCounter = attackTime;
+        attacking = true;
+        rb2D.velocity = Vector2.zero;
+        anim.SetBool("isAttacking", true);
+        //THE CHANGES TO WORKING CODE
+
+        Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+        for (int i = 0; i < enemiesToDamage.Length; i++)
+        {
+            enemiesToDamage[i].GetComponent<EnemyFollow>().TakeDamage(damage);
+        }
+    }
+}
---
+++ DemoAttackSoundV2/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Larry zhao: 15913026
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager _instance;
+
+    public float minPitch = 0.9f;
+    public float maxPitch = 1.1f;
+
+    public AudioSource efxSource1;
+    public AudioSource efxSource2;
+    public AudioSource efxSource3;
+    public AudioSource efxSource4;
+
+    public static SoundManager Instance
+    {
+        get { return _instance; }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public void RandomPlayAttackSource(params AudioClip[] clips)
+    {
+        float pitch = Random.Range(minPitch, maxPitch);
+        int index = Random.Range(0, clips.Length);
+        AudioClip clip = clips[index];
+        efxSource1.clip = clip;
+        efxSource1.pitch = pitch;
+        efxSource1.Play();
+    }
+
+    public void RandomPlayWalkingSource(params AudioClip[] clips)
+    {
+        float pitch = Random.Range(minPitch, maxPitch);
+        int index = Random.Range(0, clips.Length);
+        AudioClip clip = clips[index];
+
+        //BUGGED
+        efxSource2.clip = clip;
+        efxSource2.pitch = pitch;
+        efxSource2.Play();
+    }
+
+    public void RandomPlayEnemyDeadSource(params AudioClip[] clips)
+    {
+        float pitch = Random.Range(minPitch, maxPitch);
+        int index = Random.Range(0, clips.Length);
+        AudioClip clip = clips[index];
+
+        //BUGGED
+        efxSource3.clip = clip;
+        efxSource3.pitch = pitch;
+        efxSource3.Play();
+    }
+
+    public void RandomPlayEnemyTakingDemageSource(params AudioClip[] clips)
+    {
+        float pitch = Random.Range(minPitch, maxPitch);
+        int index = Random.Range(0, clips.Length);
+        AudioClip clip = clips[index];
+
+        //BUGGED
+        efxSource4.clip = clip;
+        efxSource4.pitch = pitch;
+        efxSource4.Play();
+    }
+
+}
