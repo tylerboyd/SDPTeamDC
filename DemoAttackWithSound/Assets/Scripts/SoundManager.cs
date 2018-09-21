9244b895835093072b3c653baefd0beed9303555
larry <larry.zhao@outlook.co.nz>
2018-09-21 18:39:21 +1200

    push

---
+++ DemoAttackWithSound/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Crearte by larry zhao 15913026
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager _instance;
+
+    public float minPitch = 0.9f;
+    public float maxPitch = 1.1f;
+
+    public AudioSource efxSource1;
+    public AudioSource efxSource2;
+
+    public static SoundManager Instance
+    {
+        get { return _instance; }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public void RandomPlaySource1(params AudioClip[] clips)
+    {
+        float pitch = Random.Range(minPitch, maxPitch);
+        int index = Random.Range(0, clips.Length);
+        AudioClip clip = clips[index];
+        efxSource1.clip = clip;
+        efxSource1.pitch = pitch;
+        efxSource1.Play();
+    }
+
+    public void RandomPlaySource2(params AudioClip[] clips)
+    {
+        float pitch = Random.Range(minPitch, maxPitch);
+        int index = Random.Range(0, clips.Length);
+        AudioClip clip = clips[index];
+
+        efxSource2.clip = clip;
+        efxSource2.pitch = pitch;
+        efxSource2.Play();
+    }
+
+
+}
