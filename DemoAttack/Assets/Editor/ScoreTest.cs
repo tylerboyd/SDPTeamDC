d259d44aa7b0bc398d0d0fa76945bdfc1521fae8
ABycroft <42327894+ABycroft@users.noreply.github.com>
2018-09-24 17:52:07 +1200

    Unit testing

---
+++ DemoAttack/Assets/Editor/ScoreTest.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class ScoreTest
+{
+
+    [Test]
+    public void ScoreShouldIncreaseTest()
+    {
+        PlayerScore playerScore = new PlayerScore();
+        var originalScore = playerScore.score;
+
+        playerScore.SendMessage("AddScore");
+
+        Assert.AreEqual(originalScore + 200, playerScore.score);
+        // Use the Assert class to test conditions.
+    }
+
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+    public IEnumerator ScoreTestWithEnumeratorPasses() {
+        // Use the Assert class to test conditions.
+        // yield to skip a frame
+
+        var score = GameObject.FindGameObjectWithTag("Hero");
+
+
+        Assert.AreEqual(200, score);
+
+        yield return null;
+    }
+}
