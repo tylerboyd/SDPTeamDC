2d2fdf45a6491e5e5918f03614695780e05aefcf
larryZSL <42328127+larryZSL@users.noreply.github.com>
2018-09-25 14:43:41 +1200

    Pushing the new soundManager script with new method

--- SoundManager.cs
+++ SoundManager.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//Larry zhao: 15913026
+
 public class SoundManager : MonoBehaviour
 {
     private static SoundManager _instance;
@@ -8,7 +10,11 @@ public class SoundManager : MonoBehaviour
     public float minPitch = 0.9f;
     public float maxPitch = 1.1f;
 
-    public AudioSource efxSource;
+    public AudioSource efxSource1;
+    public AudioSource efxSource2;
+    public AudioSource efxSource3;
+    public AudioSource efxSource4;
+    public AudioSource efxSource5;
 
     public static SoundManager Instance
     {
@@ -20,14 +26,62 @@ public class SoundManager : MonoBehaviour
         _instance = this;
     }
 
-    public void RandomPlay(params AudioClip[] clips)
+    public void RandomPlayAttackSource(params AudioClip[] clips)
     {
         float pitch = Random.Range(minPitch, maxPitch);
         int index = Random.Range(0, clips.Length);
         AudioClip clip = clips[index];
-        efxSource.clip = clip;
-        efxSource.pitch = pitch;
-        efxSource.Play();
+        efxSource1.clip = clip;
+        efxSource1.pitch = pitch;
+        efxSource1.Play();
+    }
+
+    public void RandomPlayWalkingSource(params AudioClip[] clips)
+    {
+        float pitch = Random.Range(minPitch, maxPitch);
+        int index = Random.Range(0, clips.Length);
+        AudioClip clip = clips[index];
+
+        //BUGGED
+        efxSource2.clip = clip;
+        efxSource2.pitch = pitch;
+        efxSource2.Play();
+    }
+
+    public void RandomPlayEnemyDeadSource(params AudioClip[] clips)
+    {
+        float pitch = Random.Range(minPitch, maxPitch);
+        int index = Random.Range(0, clips.Length);
+        AudioClip clip = clips[index];
+
+        //BUGGED
+        efxSource3.clip = clip;
+        efxSource3.pitch = pitch;
+        efxSource3.Play();
+    }
+
+    public void RandomPlayEnemyTakingDamageSource(params AudioClip[] clips)
+    {
+        float pitch = Random.Range(minPitch, maxPitch);
+        int index = Random.Range(0, clips.Length);
+        AudioClip clip = clips[index];
+
+        //BUGGED
+        efxSource4.clip = clip;
+        efxSource4.pitch = pitch;
+        efxSource4.Play();
+    }
+
+    public void RandomPlayHeroTakingDamageSource(params AudioClip[] clips)
+    {
+        float pitch = Random.Range(minPitch, maxPitch);
+        int index = Random.Range(0, clips.Length);
+        AudioClip clip = clips[index];
+
+        //BUGGED
+        efxSource5.clip = clip;
+        efxSource5.pitch = pitch;
+        efxSource5.Play();
     }
 
 }
