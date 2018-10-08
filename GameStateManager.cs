ccd0e39ca804cabbdcb8474b860d8341cc729808
larryZSL <42328127+larryZSL@users.noreply.github.com>
2018-10-09 09:06:02 +1300

    These are the scripts for game pause function
    Need to add a new game object named gameStateManager to control the state of the game, then add another button object under the canvas object to be a pause button.

---
+++ GameStateManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+    Running,
+    Pause
+}
+
+public class GameStateManager : MonoBehaviour {
+    public static GameStateManager _instance;
+
+    public GameState gameState = GameState.Running;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void ChangeGameState()
+    {
+        if(gameState == GameState.Running)
+        {
+            GamePause();
+        }
+        else if(gameState == GameState.Pause)
+        {
+            ContinueGame();
+        }
+    }
+
+    public void GamePause()
+    {
+        Time.timeScale = 0;
+        gameState = GameState.Pause;
+    }
+
+    public void ContinueGame()
+    {
+        Time.timeScale = 1;
+        gameState = GameState.Running;
+    }
+
+
+
+}
