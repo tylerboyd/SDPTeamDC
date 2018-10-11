3e698d7180d038b134dec17470d4efa54b96dd04
tylerboyd <36950595+tylerboyd@users.noreply.github.com>
2018-10-11 14:40:06 +1300

    Added Store UI + Consumable Bar
    Store UI is implemented, just needs functionality.
    Consumable bar is implemented, also needs functionality.
    Changed position of score and gold counter during game.

---
+++ TeamDC - Sprint1 - Dungeon Crawler/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Tobias McGee: 1323946
+//Andrew Bycroft: 16948980
+
+public class PlayerHealth : MonoBehaviour {
+
+    public Rigidbody2D rb2D;
+    public HealthSystem healthSystem;
+    Animator anim;
+
+    public int maxHealth;
+    public GameObject bloodSplash;
+    public float attackTime;
+    private float attackTimeCounter;
+    private bool attacking;
+
+    //variables for invincibility frames
+    private bool flashing;
+    public float flashingLength;
+    private float flashCounter;
+    private SpriteRenderer playerSprite;
+
+    public AudioClip heroTakingDamageSound1;
+    public AudioClip heroTakingDamageSound2;
+
+    // Use this for initialization
+    void Start () {
+        rb2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        healthSystem.SetUp(maxHealth);
+        playerSprite = GetComponent<SpriteRenderer>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        //If health reaches 0, kill player and send to game over screen
+        if (healthSystem.GetHealth() <= 0)
+        {
+            //TODO: switch scene to Game Over
+            Destroy(gameObject);
+
+            //SoundManager.Instance.RandomPlayEnemyDeadSource(enemyDeadSound1, enemyDeadSound2);
+            Debug.LogFormat(gameObject.name + " was killed");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + -1);
+        }
+
+        if (attackTimeCounter > 0)
+        {
+            attackTimeCounter -= Time.deltaTime;
+        }
+
+        //Flash invincibility frames
+        if(flashing)
+        {   
+            //sprite should be invisible if the flash counter is above 0.66 seconds
+            //two-thirds of a second
+            if(flashCounter > flashingLength * .66f)
+            {
+                playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 0.5f);
+            }
+            //one-third of a second
+            else if(flashCounter > flashingLength * 0.33f)
+            {
+                playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 1f);
+            }
+            else if (flashCounter > 0)
+            {
+                playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 0.5f);
+            }
+            else
+            {
+                /*  Once player flashCounter reaches 0, turn off flashing and set opacity to full                 
+                 */
+                playerSprite.color = new Color(playerSprite.color.r, playerSprite.color.g, playerSprite.color.b, 1f);
+                flashing = false;
+            }
+
+            flashCounter -= Time.deltaTime;
+        }
+
+    }
+
+    public void TakeDamage(int damage)
+    {
+        //Only let the player take damage when the counter is at zero
+        if (attackTimeCounter <= 0)
+        {
+            attackTimeCounter = attackTime;
+            attacking = true;
+            Instantiate(bloodSplash, transform.position, Quaternion.identity);
+            healthSystem.Damage(damage);
+
+            //Tims
+            flashing = true;
+            flashCounter = flashingLength;
+
+            SoundManager.Instance.RandomPlayHeroTakingDamageSource(heroTakingDamageSound1, heroTakingDamageSound2);
+            Debug.LogFormat("Damage dealt to " + gameObject.name);
+        }
+    }
+}
---
+++ TeamDC - Sprint1 - Dungeon Crawler/Assets/Scripts/PlayerScore.cs
@@ -0,0 +1,81 @@
+﻿//Tarran O'Shaughness hcv3389
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScore : MonoBehaviour {
+
+    public GameObject playerscores;
+    
+    public Text ScoreText;
+    public Text GoldText;
+    public int score;
+    public int gold;
+
+	void Awake () {
+        SetScoreText();
+        SetGoldText();
+	}
+	
+    void SetScoreText()
+    {
+        ScoreText.text = "Score: " + score.ToString();
+    }
+
+    void AddScore()
+    {
+        score = score + 200;
+        SetScoreText();
+    }
+
+    public int AddScore_Test(int score)
+    {
+        score = score + 200;
+        return score;
+    }
+
+    void SetGoldText()
+    {
+        GoldText.text = ": " + gold.ToString();
+    }
+
+    void AddGold()
+    {
+        gold = gold + 20;
+        SetGoldText();
+    }
+
+    public int AddGold_Test(int gold)
+    {
+        gold = gold + 20;
+        return gold;
+    }
+
+    /*void OnGUI()for testing
+    {
+        if (GUI.Button(new Rect(10, 100, 100, 30), "Score up"))
+        {
+            AddScore();
+        }
+        if (GUI.Button(new Rect(10, 140, 100, 30), "Gold up"))
+        {
+            AddGold();
+        }*/
+}
+
+   /* void OnCompletion()
+    {
+        if//user is defeated
+        {
+            PlayerInfo.info.gold = PlayerInfo.info.gold + gold;
+            if (PlayerInfo.info.highscore < score)
+            {
+                PlayerInfo.info.highscore = score;
+                PlayerInfo.info.Save();
+            }
+            PlayerInfo.info.Save();
+        }
+    }
+}*/
