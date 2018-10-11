e0e68b3de16a04d7e4601a4b6be7210fb123c297
Tarran O'Shaughnessy <tarran.oshaughnessy@gmail.com>
2018-10-11 17:16:57 +1300

    Added Inventory System
    Added an inventory system that caps at three items, as well as purchasing items from the shop.

---
+++ TeamDC - Sprint1 - Dungeon Crawler/Assets/Scripts/PlayerScore.cs
@@ -0,0 +1,67 @@
+﻿//Tarran O'Shaughness hcv3389
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScore : MonoBehaviour {
+
+    public GameObject playerscores;
+    
+    public Text ScoreText;
+    public Text GoldText;
+    public int score;
+    public int gold;
+
+	void Awake () {
+        SetScoreText();
+        SetGoldText();
+	}
+	
+    void SetScoreText()
+    {
+        ScoreText.text = "Score: " + score.ToString();
+    }
+
+    void AddScore()
+    {
+        score = score + 200;
+        SetScoreText();
+    }
+
+    public int AddScore_Test(int score)
+    {
+        score = score + 200;
+        return score;
+    }
+
+    void SetGoldText()
+    {
+        GoldText.text = ": " + gold.ToString();
+    }
+
+    void AddGold()
+    {
+        gold = gold + 20;
+        SetGoldText();
+    }
+
+    public int AddGold_Test(int gold)
+    {
+        gold = gold + 20;
+        return gold;
+    }
+
+    void OnCompletion()
+    {
+        PlayerInfo.FindObjectOfType<PlayerInfo>().gold = PlayerInfo.FindObjectOfType<PlayerInfo>().gold + gold;
+        if (PlayerInfo.FindObjectOfType<PlayerInfo>().highscore < score)
+        {
+            PlayerInfo.FindObjectOfType<PlayerInfo>().highscore = score;
+            PlayerInfo.FindObjectOfType<PlayerInfo>().SendMessage("Save");
+        }
+        PlayerInfo.FindObjectOfType<PlayerInfo>().SendMessage("Save");
+        Debug.LogFormat(PlayerInfo.FindObjectOfType<PlayerInfo>().highscore + "total score");
+    }
+}
