82f1f037ab5dda2f7240fd259ca33a01982b59fa
Lobotomite <42327848+Lobotomite@users.noreply.github.com>
2018-10-12 20:50:00 +1300

    Added sprites to gameover screen, merged with updated potions

---
+++ TeamDC - Sprint1 - Dungeon Crawler/Assets/Scripts/HealthPotionUpdater.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthPotionUpdater : MonoBehaviour {
+
+    public Text health_potion_count;
+
+	void Awake ()
+    {
+        PlayerInfo.FindObjectOfType<PlayerInfo>().SendMessage("HealthPotionTotal");
+        SetHealthPotionCount();
+    }
+
+    void SetHealthPotionCount()
+    {
+        health_potion_count.text = PlayerInfo.FindObjectOfType<PlayerInfo>().healthpotions + "x";
+    }
+}
---
+++ TeamDC - Sprint1 - Dungeon Crawler/Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,117 @@
+﻿//Tarran O'Shaughness hcv3389
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class PlayerInfo : MonoBehaviour
+{
+    public PlayerInfo info;
+    public int highscore;
+    public int gold;
+    public int healthpotions;
+    public int speedpotions;
+    public ArrayList inventory = new ArrayList();
+
+    void Awake()
+    {
+        if (info == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            info = this;
+        }
+        else if (info != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void HealthPotionTotal()
+    {
+        healthpotions = healthpotions - healthpotions;
+        for (int i = 0; i < 3; i++)
+        {
+            if (inventory[i] == "Health")
+            {
+                healthpotions = healthpotions + 1;
+            }
+        }
+    }
+
+    void RemoveHealthPotion()
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            if(inventory[i] == "Health")
+            {
+                inventory.RemoveAt(i);
+                healthpotions = healthpotions - 1;
+                i = 3;
+            }
+        }
+        HealthPotionUpdater.FindObjectOfType<HealthPotionUpdater>().SendMessage("SetHealthPotionCount");
+    }
+
+    void SpeedPotionTotal()
+    {
+        speedpotions = speedpotions - speedpotions;
+        for (int i = 0; i < 3; i++)
+        {
+            if (inventory[i] == "Speed")
+            {
+                speedpotions = speedpotions + 1;
+            }
+        }
+    }
+
+    void RemoveSpeedPotion()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            if (inventory[i] == "Speed")
+            {
+                inventory.RemoveAt(i);
+                speedpotions = speedpotions - 1;
+                i = 3;
+            }
+        }
+        SpeedPotionUpdater.FindObjectOfType<SpeedPotionUpdater>().SendMessage("SetSpeedPotionCount");
+    }
+
+    public void Save()//awaiting implementation
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream fs = File.Open(Application.persistentDataPath + "/playerscores.dat", FileMode.Open);
+
+        PlayerData data = new PlayerData();
+        data.highscore = highscore;
+        data.gold = gold;
+
+        bf.Serialize(fs, data);
+        fs.Close();
+    }
+
+    public void Load()//awaiting implementation
+    {
+        if (File.Exists(Application.persistentDataPath + "/playerscores.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream fs = File.OpenRead(Application.persistentDataPath + "/playerscores.dat");
+            PlayerData data = (PlayerData)bf.Deserialize(fs);
+            fs.Close();
+
+            highscore = data.highscore;
+            gold = data.gold;
+        }
+    }
+}
+
+[System.Serializable]
+class PlayerData
+{
+    public int highscore;
+    public int gold;
+}
